ebe0577321b6cafab91c2161d6683be8a53174d4
Turq0ise <joelrsamaniego@gmail.com>
2024-03-22 21:00:56 +0800

    Mar 22 progress

--- JS/form.js
+++ JS/form.js
@@ -4,9 +4,11 @@ for(let i = 0; i < getFormItems.length; i++) {
     loopItem.children.item(1).addEventListener("focus", elem => {
         elem.target.style.outline = "none";
         elem.target.previousElementSibling.style.top = "-0.75rem";
+        elem.target.previousElementSibling.style.textDecoration = "underline";
     });
     loopItem.children.item(1).addEventListener("blur", elem => {
         if(!elem.target.value) {
+            elem.target.previousElementSibling.style.textDecoration = "";
             return elem.target.previousElementSibling.style.top = "";
         };
     });
